4a40a2628e4e11ad1ec147a88ee1b99c337a0388
Zane Sterling <sterling.zane@gmail.com>
2013-11-30 19:43:27 -0500

    Add shell functionality.

---
+++ main.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+#include "parser.c"
+#include "main.h"
+
+// TODO add support for -c 'command'
+// TODO add support for infinite lines
+// TODO abort line on ^C
+// TODO close on ^D
+
+int main() {
+	char ** tokenList, line[1024];
+	int pid, s;
+
+	if (signal(SIGINT, sigHandler) == SIG_ERR) {
+		printf("SIGINT handler failed");
+		exit(-1);
+	}
+
+	if (signal(SIGQUIT, sigHandler) == SIG_ERR) {
+		printf("SIGQUITE handler failed");
+		exit(-1);
+	}
+
+	while (1) {
+		//get command
+		printf("WOOP %% ");
+		getNextLine(line, 1024);
+		line[strlen(line)] = 0;
+
+		//process command
+		tokenList = getTokens(line);
+		tokenList = cleanTokenList(tokenList);
+
+		if (!strcmp(tokenList[0], "exit")) {
+			exit(0);
+		}
+
+		//run command
+		if (tokenList[0][0]) {
+			pid = fork();
+			if (pid) { //parent
+				if (pid == -1)
+					printf("Aw, shit.");
+				wait(&s);
+			} else { //child
+				execvp(tokenList[0], tokenList);
+				printf("boash: no such command: %s\n", tokenList[0]);
+				exit(-1);
+			}
+		}
+
+		destroyTokenList(tokenList);
+		line[0] = 0;
+	}
+
+	return 0;
+}
+
+void sigHandler(int signum) {
+	int reading;
+
+	if (signum == SIGINT) {
+		printf("^C");
+	}
+
+	if (signum == SIGQUIT) {
+		fprintf(stderr, "Finally!\n");
+	}
+}
---
+++ parser.c
@@ -0,0 +1,171 @@
+#include <string.h>
+#include <stdlib.h>
+#include "parser.h"
+
+#define EOT 3
+
+// TODO deal with lines starting with >
+// TODO deal with infinitely long lines
+
+void getNextLine(char * line, int n) {
+	int reading, i, quote, dquote, esc, c;
+
+	i = 0;
+	esc = 0;
+	quote = 0;
+	dquote = 0;
+	reading = 0;
+
+	while ((c = getchar()) != EOF && i < 1023) {
+		if (c == '\'' && !dquote) {
+			quote = !quote;
+			continue;
+		}
+
+		if (c == '"' && !quote) {
+			dquote = !dquote;
+			continue;
+		}
+
+		if (esc) {
+			line[i++] = escape(c);
+			esc = 0;
+			continue;
+		}
+
+		if (c == '\\') {
+			esc = 1;
+			continue;
+		}
+
+		if (c == '\n') {
+			if (quote) {
+				printf("quote> ");
+				fflush(stdout);
+				line[i++] = '\n';
+			} else if (dquote) {
+				printf("dquote> ");
+				fflush(stdout);
+				line[i++] = '\n';
+			} else
+				break;
+		}
+
+		if (c == '\t') {
+			tabcomplete(line, n, i);
+		}
+
+		line[i++] = c;
+	}
+
+	line[i] = 0;
+}
+
+char escape(char c) {
+	switch (c) {
+		case '\\': return '\\';
+		default  : return '\0';
+	}
+
+	return 0;
+}
+
+// TODO add tab completion
+void tabcomplete(char * line, int n, int i) {
+	/*int last;
+
+	line[i+1] = 0;
+	last = strrchr(line, ' ');
+	*/
+}
+
+char ** getTokens(char * line) {
+	char ** tokens, c, * dividers;
+	int i, j, k, instring, instring2;
+
+	tokens = malloc(32 * sizeof(char *)) ;
+	dividers = " \n|><";
+	instring2 = 0;
+	instring = 0;
+	i = 0;
+	j = 0;
+	k = 0;
+
+	tokens[0] = malloc(1024 * sizeof(char));
+
+	for (i = 0; line[i]; i++) {
+		c = line[i];
+
+		// deal with string-related things
+		if (c == '"' && !instring2) {
+			instring = !instring;
+			continue;
+		}
+
+		if (c == '\'' && !instring) {
+			instring2 = !instring2;
+			continue;
+		}
+
+		// TODO handle ending in the middle of a string
+		// tokens in a string are not evaluated
+		if (instring || instring2) {
+			tokens[j][k++] = c;
+			continue;
+		}
+
+		// non-string non-dividers can pass
+		if (!strchr(dividers, c)) {
+			tokens[j][k++] = c;
+			continue;
+		}
+
+		// > must be treated specially because >>
+		if (c == '>' && tokens[j-1][0] == '>') {
+			tokens[j-1][1] = c;
+			tokens[j-1][2] = 0;
+			continue;
+		}
+
+		// dividers are put in their own token
+		tokens[++j] = malloc(1024 * sizeof(char));
+		tokens[j][k=0] = c;
+		tokens[j++][++k] = 0;
+		tokens[j] = malloc(1024 * sizeof(char));
+		k=0;
+	}
+
+	tokens[j][k] = 0;
+	tokens[j+1] = NULL; // terminate the list of args
+
+	return tokens;
+}
+
+char ** cleanTokenList(char ** tokenList) {
+	char ** tl;
+	int len, i;
+
+	len = 0;
+	while (tokenList[len++]);
+	len--;
+	len = (len + 1) / 2;
+
+	tl = malloc((len + 1) * sizeof(char *));
+	for (i = 0; i < len; i++) {
+		tl[i] = malloc(1024 * sizeof(char));
+		strncpy(tl[i], tokenList[i*2], 1023);
+		tl[i][1023] = 0;
+	}
+
+	tl[len] = NULL;
+
+	return tl;
+}
+
+void destroyTokenList(char ** tokenList) {
+	int i;
+	for (i = 0; tokenList[i]; i++)
+		free(tokenList[i]);
+	tokenList[0] = 0;
+	free(tokenList);
+}
